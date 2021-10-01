af62ae777fa10244ee96389fa0742d05ab4a853a
pablo99nunez <pablo99nunez@gmail.com>
2021-10-01 16:09:11 -0300

    v058

--- script.js
+++ script.js
@@ -11,13 +11,35 @@ $(".Arrowdown, .link").on("click",function(e){
 })
 
 
+function menu(x) {
 
-linkCont.forEach((linkC) => {
-    
-    linkC.addEventListener("mouseenter",()=>{
-        linkC.children[1].style.display="block";
-    })
-    linkC.addEventListener("mouseleave",()=>{
-        linkC.children[1].style.display="none";
-    })
-})
+    if(!x.matches){
+
+        linkCont.forEach((linkC) => {
+            
+            linkC.addEventListener("mouseenter", () => {
+                linkC.children[1].classList.remove("disable");
+            })
+            linkC.addEventListener("mouseleave", () => {
+                linkC.children[1].classList.add("disable");
+            })
+        })
+    }else{
+        const tap  =document.querySelector(".tap");
+        tap.classList.toggle("disable")
+        document.querySelectorAll(".Arrowdown").forEach((e)=>{
+            e.style.animation="jump 1s ease-in-out infinite";
+        })
+        document.addEventListener("click",()=>{
+            tap.classList.add("disable");
+            linkCont.forEach((linkC) => {
+                
+                linkC.children[1].classList.toggle("disable");
+                linkC.children[0].classList.toggle("invisible")
+            })
+                
+        })
+    }
+}
+var x=window.matchMedia("(max-width:500px)")
+menu(x);
